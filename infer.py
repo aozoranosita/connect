88e0e3015f7c2ca13d9a0671b1b25ca9db13774e
haruto yoshimura <sense8west@icloud.com>
2024-07-10 15:54:11 +0900

    complete infer but affinity all 0? trouble

--- infer.py
+++ infer.py
@@ -8,12 +8,30 @@ from model import ResidualSymmetricUNet3D
 import imageio
 
 MAX_SIZE = (128,128,128)
-DATA = "../image/test-input.tif"
+DATA = "image/test-input.tif"
 WEIGHT  = 'residual_symmetric_unet3d.pth'
 SAVE_PATH = "../pred/test.npy"
 
-def fitsize(imgsize, max_size):
-
+#mininum ver for x,y > MAX_SIZE
+def fitsize(imgsize, max_size): # minimun size is 16 if model is depth of 4
+    x, y, z = imgsize
+    X, Y, Z = max_size
+    nz = z // Z + 1
+    sizez = (z // nz // 16) * 16 #all vertecies need to be 16N
+    ny = y // Y 
+    nx = x // X
+    glid = []
+    px, py, pz = 0, 0, 0
+    for _ in range(nz):
+        for _ in range(ny):
+            for _ in range(nx):
+                glid.append((px, px + X, py, py + Y, pz, pz + sizez))
+                px += X
+            py += Y
+            px = 0
+        pz += sizez
+        py = 0
+    return glid
 
 if __name__=="__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -24,12 +42,11 @@ if __name__=="__main__":
     model.load_state_dict(torch.load(WEIGHT))
 
     image = imageio.volread(DATA) #big block
-
+    imgshape = image.shape
+    glid = fitsize(imgshape, MAX_SIZE)
     # input image is size 128**3, no overlap
-    # minimun size is 16 if model is depth of 4
 
-
-    list_aff = []
+    arr = np.zeros((3,) + imgshape, dtype=np.uint8)
     for pos in glid:
         x, x_, y, y_, z, z_ = pos
         img = image[x:x_, y,y_, z,z_]
@@ -39,7 +56,7 @@ if __name__=="__main__":
             input = torch.tensor(img, dtype=torch.float16).unsqueeze(0)
             affinity_map = model(input)
         
-        list_aff.append(affinity_map)
+        arr[:, x:x_, y,y_, z,z_] = affinity_map
 
     
-    np.save(affinity_maps, "../pred/test.npy")
+    np.save("../pred/test.npy", arr)
